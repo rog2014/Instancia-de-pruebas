8cd55c6751950a8441a4a1ca31edd099c37b2cef
rog2014 <rogerghv@gmail.com>
2021-03-14 20:35:18 -0600

    dele

--- models/stock_inventory.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class StockInventory(models.Model):
-    _inherit = 'stock.inventory'
-
-    wc_instance_id = fields.Many2one("wc.instance.cft", "WooCommerce Instance")
